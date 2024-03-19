aa0671664b36268b54b152a4a5f645152b061904
snzlbo <sainzolboo619@gmail.com>
2024-03-19 17:19:16 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify()],
-  build: {
-    lib: {
-      entry: "./src/main.ts",
-      name: "MultiChatEmbed",
-      fileName: "widget",
-      formats: ["es"],
-    },
-  },
   define: {
     "process.env.NODE_ENV": '"production"',
   },
